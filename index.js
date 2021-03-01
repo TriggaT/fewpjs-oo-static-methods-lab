8f30186e6d99b8e7d43d458cc49a056176989a30
Trayshawn <traytwebb14@yahoo.com>
2021-03-01 16:39:32 -0600

    commit tree 82f0b519cf10ae2ab6b409c8208a44d68ccc7ec8

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+
+  }
+
+  static titleize(string){
+    let sentence = string.split(" ")
+
+    let nonCaps = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    return sentence.map(e => {
+      if (!nonCaps.includes(e) || e === sentence[0]){
+        return Formatter.capitalize(e)
+      }
+      else return e
+    }).join(" ")
+
+
+
+
+  }
 }
